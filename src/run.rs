22b9f1d091d286b4ba31dbe2c04ef3ea0f9dd018
Dmitri9149 <dmitri.as.name@gmail.com>
2024-06-21 19:09:01 +0300

    print interpreter version + how to exit

--- src/run.rs
+++ src/run.rs
@@ -3,8 +3,10 @@ use rustyline::DefaultEditor;
 const HISTORY_FILE_PATH: &str = "./.own_lisp_history.txt";
 
 pub fn repl() -> Result<(), Box<dyn std::error::Error>> {
-    let mut rl = DefaultEditor::new()?;
+    println!("Interpreter Version {}", "0.0.0.0.1");
+    println!("Press Ctrl+c or Ctrl+d to Exit\n");
 
+    let mut rl = DefaultEditor::new()?;
     if rl.load_history(HISTORY_FILE_PATH).is_err() {
         println!("No history found");
     }
