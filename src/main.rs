6d6a6bff4820eea50e1e74f4f47538f38f4f628a
Dmitri9149 <dmitri.as.name@gmail.com>
2024-06-24 17:21:01 +0300

    use run function instead of repl

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use own_lisp::run::repl;
+use own_lisp::run::{repl,run};
 use own_lisp::error;
 use std::process::exit;
 
 fn main() {
-    if let Err(e) = repl() {
+    if let Err(e) = run() {
         eprintln!("Error {}", e);
         exit(1);
     }
