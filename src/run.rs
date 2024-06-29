0063c335d93cee3da26ea5fa60c38af8dcd68b92
Dmitri9149 <dmitri.as.name@gmail.com>
2024-06-29 17:54:48 +0300

    refactor the code : delete unused comments, use bool valued configuration.readline.save_history

--- src/run.rs
+++ src/run.rs
@@ -5,9 +5,6 @@ use log::{debug, info, warn, error};
 use pretty_env_logger;
 use rustyline::error::ReadlineError;
 use std::env::set_var;
-// use env_logger::Env;
-
-// const HISTORY_FILE_PATH: &str = "./.own_lisp_history.txt";
 
 pub fn repl() -> Result<()> {
 
@@ -20,33 +17,12 @@ pub fn repl() -> Result<()> {
     if rl.load_history(&configuration.history_file).is_err() {
         info!("No history found");
     }
-    // ask user to save or not history of the session in history file 
-    // for future use 
-    info!("To save history of the session to history file, print 'y' + Enter");
-
-    let rl_question = rl
-      .readline(&configuration.readline.prompt);
-    let yes = "y".to_string();
     
-    let mut save_to_file:bool = false;
-    match rl_question {
-      Err(ReadlineError::Interrupted) => {
-        info!("CTRL-C");
-        std::process::exit(0);
-      }
-      Err(ReadlineError::Eof) => {
-        info!("CTRL-D");
-        std::process::exit(0);
-      },
-      Ok(res) => 
-        if res == yes {
-          info!("Inputs will be saved to history file");
-          save_to_file = true;
-        } 
-        else  {
-          info!("Inputs will not be saved to history file");
-        }
-      _ => info!("Inputs will not be saved to history file")
+    if configuration.readline.save_history {
+      info!("Inputs will be saved to history file");
+    } 
+    else  {
+      info!("Inputs will not be saved to history file");
     }
 
     loop {
@@ -72,7 +48,7 @@ pub fn repl() -> Result<()> {
 
         }
     }
-    if save_to_file  {rl.save_history(&configuration.history_file)?};
+    if configuration.readline.save_history  {rl.save_history(&configuration.history_file)?};
 
     Ok(())
 }
@@ -85,11 +61,7 @@ pub fn run() -> Result<()> {
   set_var("RUST_LOG", configuration.readline.log_level);
   pretty_env_logger::init();
 
-//  let env = Env::default().default_filter_or("info");
-//  env_logger::Builder::from_env(env).init();
-
   repl()?;
 
   Ok(())
-
 }
