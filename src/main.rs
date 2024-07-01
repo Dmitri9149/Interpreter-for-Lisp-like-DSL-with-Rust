d853cdc5cb6ed281f0966cc5f536955896955851
Dmitri9149 <dmitri.as.name@gmail.com>
2024-07-01 18:03:21 +0300

    add 'simple_examples' function to print parse examples of our grammar

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,14 @@
 use own_lisp::run::run;
 use std::process::exit;
 
+use own_lisp::parse;
+
 
 
 fn main() {
+
+    parse::simple_examples();
+
     if let Err(e) = run() {
         eprintln!("Error {}", e);
         exit(1);
